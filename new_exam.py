ba45f939996a97ffe567b71df604acf7b228a719
Stephen Mullens <55811438+srmullens@users.noreply.github.com>
2020-08-12 13:31:36 -0400

    Remove example code sections

--- new_exam.py
+++ new_exam.py
@@ -41,16 +41,10 @@ true_false_questions = [
 # Special changes to the True/False section.
 #
 
-### Index 5: "T/F? The atmosphere is layered in this order: ... ?"
-order = true_false_questions[5]['options']
-random = random_sequence(order)
-    # Put the randomized order in a sentence.
-sentence = random[0] + ", " + random[1] + ", " + random[2] + ", and " + random[3]
-    # Put the sentence as the option.
-true_false_questions[5]['options'] = [sentence]
-    # Adjust whether the answer is correct or not.
-for i,item in enumerate(order):
-    if random[i] != order[i]: true_false_questions[5]['answers'] = [False]
+### Write some python 3 code to make special adjustments to one of the 
+### questions, options, answers. This is useful when the typical randomization
+### options provided in exam_functions.py aren't what you want for a specific
+### question.
 
 
 
@@ -82,27 +76,10 @@ multiple_choice_questions = [
 # Special changes to the Multiple Choice section.
 #
 
-### Index 12: An air parcel moves up to OPTION. What temperature will it have?
-    # Get the options for question wording and list of answer choices.
-options = multiple_choice_questions[12]['options']
-choices = multiple_choice_questions[12]['choices']
-    # Select what the question will say.
-option = random_choice(options)
-    # Based on the chosen question wording, get the correct answer.
-answer = multiple_choice_questions[12]['answer'][options.index(option)]
-print(answer)
-    # Based on the chosen question wording, change the last two answer choices.
-choices[3] = answer
-if options.index(option)==0:
-    choices[2] = '8C'
-else:
-    index_two = str(30-(10*(options.index(option)+1)))+'C'
-    choices[2] = index_two
-
-    # Put items back in the list.
-multiple_choice_questions[12]['options'] = [option]
-multiple_choice_questions[12]['choices'] = choices
-multiple_choice_questions[12]['answer'] = [answer]
+### Write some python 3 code to make special adjustments to one of the 
+### questions, options, choices, answers. This is useful when the typical randomization
+### options provided in exam_functions.py aren't what you want for a specific
+### question.
 
 
 
@@ -110,4 +87,3 @@ multiple_choice_questions[12]['answer'] = [answer]
 
 # Create the exam
 create_the_exam(true_false_questions,multiple_choice_questions)
-
